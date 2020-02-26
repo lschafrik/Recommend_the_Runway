76082a76e60e19958defcd31b72a462e05d40112
Lauren Schafrik <laurenabigail1993@gmail.com>
2020-02-26 18:45:28 -0500

    pickled model, updated class w pickle file

--- c3_rec-class.py
+++ c3_rec-class.py
@@ -1,41 +1,40 @@
-# if user previous rating count >= 15:
-
-    # take top 'n' rated items
-        # find top 'n' most similar not-yet-rated-by-user items from item_matrix
-            # predict rating on these items based on user's previous ratings
-                # if rating >= 9, recommend item
-
 from surprise import SVD, accuracy
 from surprise.model_selection import train_test_split
 from surprise import Dataset
 from surprise.reader import Reader
+from surprise.dump import load
 
 import numpy as np
 import pandas as pd
 
-item_cat_df = pd.read_csv('data/item_category.csv')
 
+surprise_model = load("data/svd_garment_model.pickle")
+algo = surprise_model[1]
+preds = surprise_model[0]
 ratings_df = pd.read_csv('data/ratings_df.csv')
 
+item_cat_df = pd.read_csv('data/item_category.csv')
 # items_matrix = pd.read_csv('data/item_cos_matrix.csv')
+
 
 class garmentRecommender():
 
-    def __init__(self, model, ratings_matrix, items_matrix=None):
+    def __init__(self, model=surprise_model, ratings_matrix=ratings_df, items_matrix=None):
         self.model = model
+        self.algo = self.model[1]
         self.ratings_matrix = ratings_matrix
         self.items_matrix = items_matrix
 
-    def translate(self, r_scale):
-        self.r_scale = r_scale
+    # def translate(self, r_scale):
+    #     self.r_scale = r_scale
 
-        reader = Reader(rating_scale=r_scale)
-        data = Dataset.load_from_df(self.ratings_matrix, reader)
+    #     reader = Reader(rating_scale=r_scale)
+    #     data = Dataset.load_from_df(self.ratings_matrix, reader)
 
-        self.trainset = data.build_full_trainset()
+    #     self.trainset = data.build_full_trainset()
 
-    def fit(self):
-        return self.model.fit(self.trainset)
+    # def fit(self):
+    #     return self.model.fit(self.trainset)
 
     def predict(self, usr_id, n=10):
         self.usr_id = usr_id
@@ -43,7 +42,7 @@ class garmentRecommender():
         self.predictions = []
 
         for itm in self.item_lst:
-            self.predictions.append(self.model.predict(usr_id, itm))
+            self.predictions.append(self.algo.predict(usr_id, itm))
         
         self.predictions = pd.DataFrame(self.predictions)[['uid', 'iid', 'est']].sort_values(by='est',
                                                                                              ascending=False)[0:n]
@@ -65,14 +64,14 @@ class garmentRecommender():
 
 if __name__ == "__main__":
 
-    model = SVD(lr_all=0.005, n_epochs=20, n_factors=25, reg_all=0.1)
-    recommender = garmentRecommender(model, ratings_df)
+    # model = SVD(lr_all=0.005, n_epochs=20, n_factors=25, reg_all=0.1)
+    recommender = garmentRecommender()
 
     # print(recommender.translate((2,10)))
-    recommender.translate((2,10))
+    # recommender.translate((2,10))
 
     # print(recommender.fit())
-    recommender.fit()
+    # recommender.fit()
 
     # print(recommender.predict(691468, 5))
     recommender.predict(691468, 5)
