fff3c28cd280a0f20fcf042c547e853c4bdc8926
Lauren Schafrik <laurenabigail1993@gmail.com>
2020-02-27 15:06:26 -0500

    updated item-item recommender in recommender class

--- c3_recommender-class.py
+++ c3_recommender-class.py
@@ -20,11 +20,12 @@ class garmentRecommender:
     Rent the Runway garment recommender
     '''
 
-    def __init__(self, model=surprise_model, ratings_matrix=ratings_df, category_data=item_cat_df):
+    def __init__(self, model=surprise_model, ratings_matrix=ratings_df, category_data=item_cat_df, item_matrix=items_matrix):
         self.model = model
         self.algo = self.model[1]
         self.ratings_matrix = ratings_matrix
         self.category_data = category_data
+        self.item_matrix = item_matrix
 
     def predict(self, usr_id, n=5):
         '''
@@ -62,6 +63,24 @@ class garmentRecommender:
         user_items = self.ratings_matrix[self.ratings_matrix['user_id'] == usr_id]['item_id']
         
         for itm in self.predict(usr_id, n)['iid']:
+            if itm not in user_items:
+                category_name = self.category_data[itm]
+                # recommended_items.append([itm, category_name])
+                recommended_items[itm] = category_name
+                
+        recommended_df =  pd.DataFrame.from_dict(recommended_items, orient='index', columns=['category'])
+        recommended_df.index.name = 'item_id'
+
+        return recommended_df
+
+    def item_recommender(self, usr_id, n=5, similarity_limit=0.99):
+        user_items = self.ratings_matrix[self.ratings_matrix['user_id'] == usr_id]
+        user_fave_itm = str(int(user_items.sort_values(by='rating', ascending=False).iloc[0].item_id))
+        similar_items_id = items_matrix[items_matrix[user_fave_itm] < similarity_limit][user_fave_itm].sort_values(ascending=False)[0:n].index
+
+        recommended_items = dict()
+
+        for itm in similar_items_id:
             if itm not in user_items:
                 category_name = self.category_data[itm]
                 # recommended_items.append([itm, category_name])
