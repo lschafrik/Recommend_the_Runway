a79d367693f224110d1557aadbc3992287da0183
Lauren Schafrik <laurenabigail1993@gmail.com>
2020-02-27 11:28:51 -0500

    added comments to recommender-class, removed c3_text

--- c3_recommender-class.py
+++ c3_recommender-class.py
@@ -9,34 +9,38 @@ import pandas as pd
 
 
 surprise_model = load("data/svd_garment_model.pickle")
-algo = surprise_model[1]
-preds = surprise_model[0]
 ratings_df = pd.read_csv('data/ratings_df.csv')
-
 item_cat_df = pd.read_csv('data/item_category.csv')
+
 # items_matrix = pd.read_csv('data/item_cos_matrix.csv')
 
 
 class garmentRecommender():
+    '''
+    Rent the Runway garment recommender
+    '''
 
-    def __init__(self, model=surprise_model, ratings_matrix=ratings_df, items_matrix=None):
+    def __init__(self, model=surprise_model, ratings_matrix=ratings_df, category_data=item_cat_df, items_matrix=None):
         self.model = model
         self.algo = self.model[1]
         self.ratings_matrix = ratings_matrix
+        self.category_data = category_data
         self.items_matrix = items_matrix
 
-    # def translate(self, r_scale):
-    #     self.r_scale = r_scale
+    def predict(self, usr_id, n=5):
+        '''
+        Takes a User ID and creates a Pandas DataFrame of rating predictions for all items
 
-    #     reader = Reader(rating_scale=r_scale)
-    #     data = Dataset.load_from_df(self.ratings_matrix, reader)
+        INPUT:
+            usr_id: INTEGER - User ID to perform rating predictions on
+            n: INTEGER - Number of rows to return
+        
+        OUTPUT:
+            Returns a Pandas DataFrame of 'n' rows of the highest rating predictions, sorted by descending values
 
-    #     self.trainset = data.build_full_trainset()
+        Notes: Column labels : Column Description = 'uid':User ID, 'iid':Item ID, and 'est':Predicted Rating
+        '''
 
-    # def fit(self):
-    #     return self.model.fit(self.trainset)
-
-    def predict(self, usr_id, n=10):
         self.usr_id = usr_id
         self.item_lst = np.unique(np.array(self.ratings_matrix['item_id']))
         self.predictions = []
@@ -46,10 +50,18 @@ class garmentRecommender():
         
         self.predictions = pd.DataFrame(self.predictions)[['uid', 'iid', 'est']].sort_values(by='est',
                                                                                              ascending=False)[0:n]
-        return self.predictions
+        # return self.predictions
 
-    def recommend_item(self, category_data):
-        self.category_data = category_data
+    def recommend_item(self):
+        '''
+        Returns the top 'n' recommended items based on ratings_matrix and the item's related related category
+
+        OUTPUT:
+            self.recommended_item: LIST OF LISTS - [[INT(item_id), STR(category)], ...]
+
+        Notes: Further descriptive information on items is currently unavailable; only recognizable by category
+        '''
+
         self.recommended_item = []
         self.user_items = np.array(self.ratings_matrix[self.ratings_matrix['user_id'] == self.usr_id]['item_id'])
         
@@ -64,16 +76,7 @@ class garmentRecommender():
 
 if __name__ == "__main__":
 
-    # model = SVD(lr_all=0.005, n_epochs=20, n_factors=25, reg_all=0.1)
     recommender = garmentRecommender()
+    recommender.predict(47002)
 
-    # print(recommender.translate((2,10)))
-    # recommender.translate((2,10))
-
-    # print(recommender.fit())
-    # recommender.fit()
-
-    # print(recommender.predict(691468, 5))
-    recommender.predict(691468, 5)
-
-    print(recommender.recommend_item(item_cat_df))
+    print(recommender.recommend_item())
