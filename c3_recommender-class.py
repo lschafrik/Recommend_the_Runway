7fd3a7ad28996a587fcee9a8750fbf5f1b72d32a
Lauren Schafrik <laurenabigail1993@gmail.com>
2020-02-27 15:38:35 -0500

    updated recommender, added comments

--- c3_recommender-class.py
+++ c3_recommender-class.py
@@ -27,13 +27,13 @@ class garmentRecommender:
         self.category_data = category_data
         self.item_matrix = item_matrix
 
-    def predict(self, usr_id, n=5):
+    def _predict(self, usr_id, n=5):
         '''
         Takes a User ID and creates a Pandas DataFrame of given user's rating predictions for all items
 
         INPUT:
             usr_id: INTEGER - User ID to perform rating predictions on
-            n: INTEGER - Number of rows to return
+            n: INTEGER(Opt) - Number of rows to return
         
         OUTPUT:
             Returns a Pandas DataFrame of 'n' rows of the highest rating predictions, sorted by descending values
@@ -48,9 +48,13 @@ class garmentRecommender:
         return pd.DataFrame(predictions)[['uid', 'iid', 'est']].sort_values(by='est',ascending=False)[0:n]
 
 
-    def recommend_item(self, usr_id, n=5):
+    def ratings_recommend(self, usr_id, n=5):
         '''
         Returns the top 'n' recommended items based on ratings_matrix and the item's related  category
+
+        INPUT:
+            usr_id: INTEGER - User ID to perform item-item recommendations on
+            n: INTEGER(Opt) - Number of rows to return
 
         OUTPUT:
             self.recommended_item: LIST OF LISTS - [[INT(item_id), STR(category)], ...]
@@ -62,7 +66,7 @@ class garmentRecommender:
         recommended_items = dict()
         user_items = self.ratings_matrix[self.ratings_matrix['user_id'] == usr_id]['item_id']
         
-        for itm in self.predict(usr_id, n)['iid']:
+        for itm in self._predict(usr_id, n)['iid']:
             if itm not in user_items:
                 category_name = self.category_data[itm]
                 recommended_items[itm] = category_name
@@ -72,7 +76,21 @@ class garmentRecommender:
 
         return recommended_df
 
-    def item_recommender(self, usr_id, n=5, similarity_limit=0.99):
+    def item_recommend(self, usr_id, n=5, similarity_limit=0.99):
+        '''
+        Takes a User ID and creates a Pandas DataFrame of item recommendations based on user's highest-rated item_id's similarity with other items in item_matrix
+
+        INPUT:
+            usr_id: INTEGER - User ID to perform item-item recommendations on
+            n: INTEGER(Opt) - Number of rows to return
+            similarity_limit: FLOAT(Opt) - Similarity limitation to specify how similar recommendations should be
+        
+        OUTPUT:
+            Returns a Pandas DataFrame of 'n' rows of recommendations based on highest similarity to user's highest-rated item, sorted by descending values
+
+        Notes: Further descriptive information on items is currently unavailable; only recognizable by category
+        '''
+
         user_items = self.ratings_matrix[self.ratings_matrix['user_id'] == usr_id]
         user_fave_itm = str(int(user_items.sort_values(by='rating', ascending=False).iloc[0].item_id))
         similar_items_id = items_matrix[items_matrix[user_fave_itm] < similarity_limit][user_fave_itm].sort_values(ascending=False)[0:n].index
@@ -82,7 +100,7 @@ class garmentRecommender:
         for itm in similar_items_id:
             if int(itm) not in user_items:
                 category_name = self.category_data[int(itm)]
-                recommended_items[int(itm)] = category_name
+                recommended_items[itm] = category_name
                 
         recommended_df =  pd.DataFrame.from_dict(recommended_items, orient='index', columns=['category'])
         recommended_df.index.name = 'item_id'
@@ -91,10 +109,11 @@ class garmentRecommender:
 
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-    # recommender = garmentRecommender()
-    # example_user = 47002
-    # print(recommender.predict(example_user))
+    recommender = garmentRecommender()
+    example_user = 47002
 
-    # print(recommender.recommend_item(example_user))
+    print(recommender.ratings_recommend(example_user))
+
+    print(recommender.item_recommend(example_user))
