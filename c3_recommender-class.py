a4e0f517efc4ab1952df354bb05671e6c2f8f81e
Lauren Schafrik <laurenabigail1993@gmail.com>
2020-02-28 18:42:20 -0500

    added fun interface and pictures

--- c3_recommender-class.py
+++ c3_recommender-class.py
@@ -6,6 +6,7 @@ from surprise.dump import load
 
 import numpy as np
 import pandas as pd
+import time
 
 
 surprise_model = load("data/svd_garment_model.pickle")
@@ -14,6 +15,8 @@ item_cat_df = pd.read_csv('data/item_category.csv').set_index('item_id')['catego
 
 items_matrix = pd.read_csv('data/item_cos_matrix.csv')
 items_matrix.set_index(items_matrix.columns, inplace=True)
+
+# nmf_model = pd.read_csv('data/nmf_matrix.csv')
 
 class garmentRecommender:
     '''
@@ -44,6 +47,7 @@ class garmentRecommender:
         item_lst = self.ratings_matrix['item_id'].unique()
         
         predictions = [self.algo.predict(usr_id, itm) for itm in item_lst]
+
         
         return pd.DataFrame(predictions)[['uid', 'iid', 'est']].sort_values(by='est',ascending=False)[0:n]
 
@@ -77,7 +81,8 @@ class garmentRecommender:
 
     def recommend_onitems(self, usr_id, n=5, similarity_limit=0.99):
         '''
-        Takes a User ID and creates a Pandas DataFrame of item recommendations based on user's highest-rated item_id's similarity with other items in item_matrix
+        Takes a User ID and creates a Pandas DataFrame of item recommendations based 
+        on user's highest-rated item_id's similarity with other items in item_matrix
 
         INPUT:
             usr_id: INTEGER - User ID to perform item-item recommendations on
@@ -106,8 +111,94 @@ class garmentRecommender:
 
         return recommended_df
 
+    # def recommend_nmf(self, usr_id, n=5):
+    #     '''
+    #     Takes a User ID and creates a Pandas DataFrame of item recommendations based 
+    #     on user's highest-rated item_id's similarity with other items in item_matrix
+
+    #     INPUT:
+    #         usr_id: INTEGER - User ID to perform item-item recommendations on
+    #         n: INTEGER(Opt) - Number of rows to return
+        
+    #     OUTPUT:
+    #         Returns a Pandas DataFrame of 'n' rows of recommendations based on highest similarity to user's highest-rated item, sorted by descending values
+
+    #     Notes: Further descriptive information on items is currently unavailable; only recognizable by category
+    #     '''
+
+    #     user_items = self.ratings_matrix[self.ratings_matrix['user_id'] == usr_id]
+    #     # user_fave_itm = str(int(user_items.sort_values(by='rating', ascending=False).iloc[0].item_id))
+    #     similar_items_id = self.nmf_model.loc[usr_id].sort_values(ascending=False) #[0:n].index ?
 
 
-# if __name__ == "__main__":
+    #     recommended_items = dict()
 
-#     recommender = garmentRecommender()
+    #     for itm in similar_items_id:
+    #         if int(itm) not in user_items:
+    #             category_name = self.category_data[int(itm)]
+    #             recommended_items[itm] = category_name
+                
+    #     recommended_df =  pd.DataFrame.from_dict(recommended_items, orient='index', columns=['category'])
+    #     recommended_df.index.name = 'item_id'
+
+    #     return recommended_df
+
+
+
+if __name__ == "__main__":
+
+    recommender = garmentRecommender()
+
+    print()
+    usr = int(input("Greetings!  Please enter a User ID :  "))
+
+    basedon_ratings = recommender.recommend_onratings(usr)
+    basedon_items = recommender.recommend_onitems(usr)
+
+    # print(basedon_ratings)
+    # print(basedon_items)
+
+    print()
+    print(f"Hello, User {usr}, and thank you for choosing Recommend the Runway!  Please hold while we find the perfect outfit for you!")
+    print()
+    time.sleep(3)
+    print("Sifting through socks...")
+    time.sleep(1)
+    print("Trying on trousers...")
+    time.sleep(1)
+    print("Contemplating coats...")
+    time.sleep(1)
+    print("Meticulously prepping mannequins...")
+    time.sleep(1)
+    print()
+    print(f"Wonderful news, User {usr}!  Based on previous ratings made by yourself and other similar users, we think you'll love\
+ Item {basedon_ratings.index[0]}.  You have great taste - it's a wonderful {basedon_ratings['category'].iloc[0] }!")
+
+    time.sleep(3)
+    print()
+    print()
+    print()
+
+    cont = input(f"Still there, User {usr}?  Would you like a different kind of recommendation (y/n)? :  ")
+    print()
+    print()
+
+    if cont == 'y':
+
+        print(f"Hello, User {usr}, and welcome back to Recommend the Runway!  Please hold while we find another perfect outfit for you!")
+        print()
+        time.sleep(3)
+        print("Ruminating over rompers...")
+        time.sleep(1)
+        print("Sorting through sweaters...")
+        time.sleep(1)
+        print("Tailoring turtlenecks...")
+        time.sleep(1)
+        print("Judging jumpsuits...")
+        time.sleep(1)
+        print()
+        print(f"What fun, User {usr}!  Based on your highest rated item, we have found another similar item we think you'll love; your taste is, as usual, exquisite -\
+ Item {basedon_items.index[0]} is a breathtaking {basedon_items['category'].iloc[0] }!")
+        time.sleep(2)
+    # else:
+        print(f"Goodbye, User {usr} - we hope to see you again soon!")
